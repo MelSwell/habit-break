950bddbdd00edcd898966c0e69ca53735475bbcc
Seth M Cohen <smcohen82@gmail.com>
2021-03-30 16:52:36 -0400

    Add encouragements functionality

--- app/controllers/encouragements_controller.rb
+++ app/controllers/encouragements_controller.rb
@@ -1,3 +1,22 @@
 class EncouragementsController < ApplicationController
+  def new
+    @encouragement = Encouragement.new
+    @habit_log = HabitLog.find(params[:habit_log_id])
+  end
+  
+  def create
+    @encouragement = Encouragement.create(encouragement_params)
+    if @encouragement.valid?
+      redirect_to habit_log_path(@encouragement.habit_log_id)
+    else
+      flash[:errors] = @encouragement.errors.full_messages
+      redirect_back fallback_location: { action: "new", habit_log_id: @encouragement.habit_log_id }
+    end
+  end
 
+  private
+
+  def encouragement_params
+    params.require(:encouragement).permit(:user_id, :comment, :habit_log_id)
+  end
 end
--- app/models/encouragement.rb
+++ app/models/encouragement.rb
@@ -1,4 +1,18 @@
 class Encouragement < ApplicationRecord
   belongs_to :user
   belongs_to :habit_log
+  validates :comment, presence: true
+
+  def formatted_time
+    time_arr = self.created_at.localtime.to_s.split
+    hour = time_arr[1].split(":")[0].to_i
+    if hour == 12
+      "#{hour}:#{time_arr[1].split(":")[1]} PM"
+    elsif hour > 12
+      hour -= 12
+      "#{hour}:#{time_arr[1].split(":")[1]} PM"
+    else
+      "#{hour}:#{time_arr[1].split(":")[1]} AM"
+    end
+  end
 end
