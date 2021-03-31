b53aebc0049a8e2c27178a2c47c6a29ab7cffdf2
Seth M Cohen <smcohen82@gmail.com>
2021-03-30 22:56:20 -0400

    Broken but close

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,6 @@
 class Habit < ApplicationRecord
   has_many :user_habits
   has_many :users, through: :user_habits
+  validates :name, :category, presence: true
+  validates :category, inclusion: { in: ["general health", "financial health", "career health", "social health", "mental health"]}
 end
--- app/models/user_habit.rb
+++ app/models/user_habit.rb
@@ -2,4 +2,5 @@ class UserHabit < ApplicationRecord
   belongs_to :user 
   belongs_to :habit 
   has_many :habit_logs
+  validates :daily_goal, :target_days, presence: true
 end
