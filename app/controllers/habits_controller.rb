0114dace5ada8b5a830eb82567acdc4f3de4125c
Henry <hvelas93@gmail.com>
2021-03-31 22:26:59 -0400

    hhenrys work

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,9 +1,19 @@
 class HabitsController < ApplicationController
+
+
+@@all_habits = Habit.all
+
   def new
     @habit = Habit.new
     @categories = Habit.all.map { |h| h.category }.uniq
     @user_habit = UserHabit.new
   end
+
+  def show
+    @habit = Habit.find(params[:id])
+    @users_of_habit = @habit.users.sort_by{ |u| u.name }
+    @specific_logs  = @habit.habit_logs.reverse
+  end 
 
   def create
     @habit = Habit.create(habit_params)
@@ -24,6 +34,16 @@ class HabitsController < ApplicationController
       flash[:errors] = @habit.errors.full_messages
       redirect_to new_habit_path
     end
+  end
+
+  def index
+    all_habits = Habit.all
+    @habits_general = all_habits.where(category: "general health").sort_by{ |h| h.users.length }.reverse!
+    @habits_financial = all_habits.where(category: "financial health").sort_by{ |h| h.users.length }.reverse!
+    @habits_social = all_habits.where(category: "social health").sort_by{ |h| h.users.length }.reverse!
+    @habits_mental = all_habits.where(category: "mental health").sort_by{ |h| h.users.length }.reverse!
+    @habits_career = all_habits.where(category: "career health").sort_by{ |h| h.users.length }.reverse!
+    
   end
 
   private
--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,7 +1,10 @@
 class Habit < ApplicationRecord
   has_many :user_habits
   has_many :users, through: :user_habits
+  has_many :habit_logs, through: :user_habits
   accepts_nested_attributes_for :user_habits
   validates :name, :category, presence: true
   validates :category, inclusion: { in: ["general health", "financial health", "career health", "social health", "mental health"]}
+
+
 end
