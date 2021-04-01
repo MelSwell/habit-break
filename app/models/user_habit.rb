b09e4b4c3aca3b9e887fb9de9b5bcbca764e23ba
Seth M Cohen <smcohen82@gmail.com>
2021-03-31 22:26:46 -0400

    Even more MVC crud functionality

--- app/models/user_habit.rb
+++ app/models/user_habit.rb
@@ -3,4 +3,8 @@ class UserHabit < ApplicationRecord
   belongs_to :habit 
   has_many :habit_logs
   validates :daily_goal, :target_days, presence: true
+
+  def toggle_active
+    self.active = !self.active
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#logout'
   resources :users 
   resources :user_habits
+  patch 'user_habits/:id/toggle_active', to: 'user_habits#toggle_active', as: 'toggle_active'
   resources :habit_logs, only: [:new, :create, :edit, :update]
   resources :friendships
   
